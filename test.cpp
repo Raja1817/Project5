181e3e8d3cdc3d31ae8dd2e18bc3d13a0a0537b0
Raja1817 <rajag9328@gmail.com>
2019-05-07 13:00:54 +0530

    second

--- test.cpp
+++ test.cpp
@@ -7,19 +7,5 @@
 #include <mongocxx/instance.hpp>
 
 int main(int, char**) {
-    mongocxx::instance inst{};
-    mongocxx::client conn{mongocxx::uri{}};
 
-    bsoncxx::builder::stream::document document{};
-
-    auto collection = conn["testdb"]["testcollection"];
-    document << "hello" << "world";
-
-    collection.insert_one(document.view());
-    auto cursor = collection.find({});
-
-    for (auto&& doc : cursor) {
-        std::cout << bsoncxx::to_json(doc) << std::endl;
-    }
-}
 
